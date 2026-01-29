a5cb4c6c3ce8db0a73e736a20278c193201fc8b4
Adisdzaky <adisdzakyrivai@gmail.com>
2026-01-29 08:22:20 +0700

    Update send.mjs

--- api/send.mjs
+++ api/send.mjs
@@ -14,7 +14,7 @@ import makeWASocket, {
   BufferJSON,
   initAuthCreds // TAMBAHKAN INI
 } from '@whiskeysockets/baileys';
-import { Redis } from '@upstash/redis';
+
 import QRCode from 'qrcode';
 import pino from 'pino';
 import axios from 'axios';
